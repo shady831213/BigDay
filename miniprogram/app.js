8db8bc1e15df624e1d11624aa6a4867837cabbc2
Apollo Li <shady831213@126.com>
2022-09-16 19:18:10 +0800

    Update app.js
    change cloud

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,21 +1,14 @@
 //app.js
-App({
-  onLaunch: function () {
-    
-    if (!wx.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
-    } else {
-      wx.cloud.init({
-        env:'release-8yu27',
-        // env 参数说明：
-        //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
-        //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
-        //   如不填则使用默认环境（第一个创建的环境）
-        // env: 'my-env-id',
-        traceUser: true,
-      })
-    }
+import { Cloud } from 'laf-client-sdk'
 
+const laf_cloud = new Cloud({
+  // the laf app server base url
+  baseUrl: "https://xofufw.lafyun.com",
+  environment: "wxmp",
+})
+App({
+  laf_cloud: laf_cloud,
+  onLaunch: function () {
     this.globalData = {}
   }
 })
